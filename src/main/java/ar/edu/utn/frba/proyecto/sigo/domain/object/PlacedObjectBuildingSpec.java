ceb08ff5d3450f84eb40c16dc2bb084d2ee219ab
cneira84 <cecilia.a.neira@gmail.com>
2017-10-28 18:18:06 -0300

    Merge branch 'master' of https://github.com/SIGO-UTN-FRBA/sigo into ceci
    # Conflicts:
    #	PlacedObjectBuildingSpec.java

--- src/main/java/ar/edu/utn/frba/proyecto/sigo/domain/object/PlacedObjectBuildingSpec.java
+++ src/main/java/ar/edu/utn/frba/proyecto/sigo/domain/object/PlacedObjectBuildingSpec.java
@@ -3,21 +3,18 @@ package ar.edu.utn.frba.proyecto.sigo.domain.object;
 
 import javax.persistence.*;
 
-import ar.edu.utn.frba.proyecto.sigo.domain.analysis.AnalysisException;
-import com.vividsolutions.jts.geom.MultiLineString;
-import com.vividsolutions.jts.geom.Polygon;
 import lombok.*;
 import com.vividsolutions.jts.geom.MultiPolygon;
 
 @Entity
 @Table(name = "public.tbl_placed_object_building_spec")
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Data
 @Builder
-
-public class PlacedObjectBuildingSpec implements PlacedObjectSpec<MultiPolygon>{
+public class PlacedObjectBuildingSpec implements PlacedObjectSpec<MultiPolygon> {
     @Id
-    @SequenceGenerator(name = "placedObjectBuildingSpecGenerator", sequenceName = "PLACED_OBJECT_BUILDING_SPEC_SEQUENCE")
+    @SequenceGenerator(name = "placedObjectBuildingSpecGenerator", sequenceName = "PLACED_OBJECT_BUILDING_SPEC_SEQUENCE", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "placedObjectBuildingSpecGenerator")
     @Column(name = "spec_id")
     private Long id;
@@ -29,6 +26,7 @@ public class PlacedObjectBuildingSpec implements PlacedObjectSpec<MultiPolygon>{
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "object_id")
     private PlacedObject placedObject;
+
 
     @Override
     public Class getGeomClass() {
