8130c24504d5662936341c54b41e1369d41e61df
cneira84 <cecilia.a.neira@gmail.com>
2017-11-04 19:30:38 -0300

    Merge branch 'master' of https://github.com/SIGO-UTN-FRBA/sigo into ceci
    # Conflicts:
    #	src/main/java/ar/edu/utn/frba/proyecto/sigo/domain/object/PlacedObjectOverheadWireSpec.java

--- src/main/java/ar/edu/utn/frba/proyecto/sigo/service/airport/RunwayDirectionService.java
+++ src/main/java/ar/edu/utn/frba/proyecto/sigo/service/airport/RunwayDirectionService.java
@@ -4,26 +4,15 @@ import ar.edu.utn.frba.proyecto.sigo.domain.airport.*;
 import ar.edu.utn.frba.proyecto.sigo.domain.airport.icao.RunwayClassificationICAOAnnex14;
 import ar.edu.utn.frba.proyecto.sigo.persistence.HibernateUtil;
 import ar.edu.utn.frba.proyecto.sigo.service.SigoService;
-<<<<<<< HEAD
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.LineString;
-=======
 import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
->>>>>>> f2185b1b2c52cc0ec7c6eae56107906dc1a73ade
 
 import javax.inject.Inject;
 import java.awt.*;
 import java.util.HashMap;
-import java.util.List;
-
-import static ar.edu.utn.frba.proyecto.sigo.utils.geom.GeometryHelper.getAzimuth;
-import static ar.edu.utn.frba.proyecto.sigo.utils.geom.GeometryHelper.getMiddle;
-import static ar.edu.utn.frba.proyecto.sigo.utils.geom.GeometryHelper.sortDirectionCoordinates;
 
 import static org.reflections.util.ConfigurationBuilder.build;
 
@@ -86,8 +75,6 @@ public class RunwayDirectionService extends SigoService<RunwayDirection, Runway>
         currentSession().save(classification);
     }
 
-<<<<<<< HEAD
-=======
     public SimpleFeature getFeature(RunwayDirection direction) {
 
         return SimpleFeatureBuilder.build(
@@ -115,5 +102,4 @@ public class RunwayDirectionService extends SigoService<RunwayDirection, Runway>
 
         return tb.buildFeatureType();
     }
->>>>>>> f2185b1b2c52cc0ec7c6eae56107906dc1a73ade
 }
