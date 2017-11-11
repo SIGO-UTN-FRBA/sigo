272d19e2d99d6bb2ed251ae74429001c4d5bd83c
cneira84 <cecilia.a.neira@gmail.com>
2017-11-11 20:32:31 -0300

    Merge branch 'master' of https://github.com/SIGO-UTN-FRBA/sigo into ceci
    # Conflicts:
    #	src/main/java/ar/edu/utn/frba/proyecto/sigo/domain/object/PlacedObjectOverheadWireSpec.java

--- src/main/java/ar/edu/utn/frba/proyecto/sigo/service/airport/RunwayDistanceHelper.java
+++ src/main/java/ar/edu/utn/frba/proyecto/sigo/service/airport/RunwayDistanceHelper.java
@@ -1,15 +1,6 @@
 package ar.edu.utn.frba.proyecto.sigo.service.airport;
 
 import ar.edu.utn.frba.proyecto.sigo.domain.airport.RunwayDirection;
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.LineString;
-
-import java.util.List;
-
-import static ar.edu.utn.frba.proyecto.sigo.utils.geom.GeometryHelper.getAzimuth;
-import static ar.edu.utn.frba.proyecto.sigo.utils.geom.GeometryHelper.getMiddle;
-import static ar.edu.utn.frba.proyecto.sigo.utils.geom.GeometryHelper.sortDirectionCoordinates;
 
 public class RunwayDistanceHelper {
 
@@ -45,24 +36,5 @@ public class RunwayDistanceHelper {
         Double threshold = direction.getApproachSection().getThresholdLength();
 
         return runway - threshold;
-    }
-
-    public static Coordinate[] getCenterRunwayPoint(RunwayDirection direction){
-        List<Coordinate> extremes = sortDirectionCoordinates(direction.getRunway().getGeom().getCoordinates(), direction.getGeom().getCoordinate());
-        Coordinate startCenterPoint = getMiddle(extremes.get(0), extremes.get(1));
-        Coordinate endCenterPoint = getMiddle(extremes.get(2), extremes.get(3));
-        return new Coordinate[]{startCenterPoint, endCenterPoint};
-    }
-
-    /*Obtengo el azimuth de la pista en base a sus puntos centrales*/
-    public static double getAzimuthRunway(RunwayDirection direction){
-        Coordinate[] centerRunwayPath = getCenterRunwayPoint(direction);
-        return getAzimuth(centerRunwayPath[0], centerRunwayPath[1]);
-    }
-
-    /*Obtengo un objeto LineString en base a sus puntos centrales*/
-    public static LineString getCenterRunwayPath(RunwayDirection direction){
-        Coordinate[] centerRunwayPath = getCenterRunwayPoint(direction);
-        return new GeometryFactory().createLineString(new Coordinate[]{centerRunwayPath[0], centerRunwayPath[1]});
     }
 }
--- src/main/java/ar/edu/utn/frba/proyecto/sigo/utils/geom/GeometryHelper.java
+++ src/main/java/ar/edu/utn/frba/proyecto/sigo/utils/geom/GeometryHelper.java
@@ -1,16 +1,12 @@
 package ar.edu.utn.frba.proyecto.sigo.utils.geom;
 
 import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.LineSegment;
 import com.vividsolutions.jts.geom.Point;
 import org.geotools.geometry.jts.JTS;
 import org.geotools.referencing.GeodeticCalculator;
 
 import javax.inject.Singleton;
 import java.awt.geom.Point2D;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Singleton
 public class GeometryHelper {
@@ -35,26 +31,6 @@ public class GeometryHelper {
         Point2D destination = gc.getDestinationGeographicPoint();
 
         return new Coordinate(destination.getX(), destination.getY());
-    }
-
-    public static Coordinate getMiddle(Coordinate startPoint, Coordinate finalPoint){
-        LineSegment segment = new LineSegment(startPoint, finalPoint);
-        return segment.pointAlong(0.5);
-    }
-
-
-    public static List<Coordinate> sortDirectionCoordinates(Coordinate[] unsortedCoordinates, Coordinate referenceCoordinate){
-        List<Coordinate> sortedCoordinates = Arrays.stream(unsortedCoordinates)
-                .distinct()
-                .sorted((i, j) -> {
-                    if (referenceCoordinate.distance(i) > referenceCoordinate.distance(j))
-                        return 1;
-                    else
-                        return -1;
-                })
-                .limit(4)
-                .collect(Collectors.toList());
-        return sortedCoordinates;
     }
 
 }
