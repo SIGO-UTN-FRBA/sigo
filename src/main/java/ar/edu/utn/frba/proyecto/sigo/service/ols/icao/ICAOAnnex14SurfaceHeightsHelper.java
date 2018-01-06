67bcd9a990cede6973beee13708bdc59290546d7
sergio087 <sergio.087@gmail.com>
2018-01-06 19:09:56 -0300

    Calculate height on transitional surface

--- src/main/java/ar/edu/utn/frba/proyecto/sigo/service/ols/icao/ICAOAnnex14SurfaceHeightsHelper.java
+++ src/main/java/ar/edu/utn/frba/proyecto/sigo/service/ols/icao/ICAOAnnex14SurfaceHeightsHelper.java
@@ -4,12 +4,16 @@ import ar.edu.utn.frba.proyecto.sigo.domain.ols.icao.ICAOAnnex14Surface;
 import ar.edu.utn.frba.proyecto.sigo.domain.ols.icao.ICAOAnnex14SurfaceConical;
 import ar.edu.utn.frba.proyecto.sigo.domain.ols.icao.ICAOAnnex14SurfaceInnerHorizontal;
 import ar.edu.utn.frba.proyecto.sigo.domain.ols.icao.ICAOAnnex14SurfaceStrip;
+import ar.edu.utn.frba.proyecto.sigo.domain.ols.icao.ICAOAnnex14SurfaceTransitional;
+import ar.edu.utn.frba.proyecto.sigo.exception.SigoException;
 import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.Point;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import javax.inject.Singleton;
+import java.util.stream.IntStream;
 
 @Singleton
 public class ICAOAnnex14SurfaceHeightsHelper {
@@ -34,7 +38,7 @@ public class ICAOAnnex14SurfaceHeightsHelper {
             case APPROACH_HORIZONTAL_SECTION:
                 break;
             case TRANSITIONAL:
-                break;
+                return this.heightAtCoordinate((ICAOAnnex14SurfaceTransitional) surface, intersection);
             case INNER_TRANSITIONAL:
                 break;
             case BALKED_LANDING_SURFACE:
@@ -49,18 +53,11 @@ public class ICAOAnnex14SurfaceHeightsHelper {
 
     private Double heightAtCoordinate(ICAOAnnex14SurfaceConical surface, Coordinate intersection){
 
-        //1. calcular minima distancia (90*) al internal ring
-        //2. aplicar relacion trigonometrica
-
-        Point point = new GeometryFactory().createPoint(intersection);
-
-        double adjacent = surface.getGeometry().getInteriorRingN(0).distance(point) * 100000;
-
-        double degrees = Math.atan(surface.getSlope() / 100);
-
-        double hypotenuse = adjacent / Math.cos(degrees);
-
-        return Math.sqrt(Math.pow(hypotenuse,2) - Math.pow(adjacent,2));
+        return heightOnSloping(
+                surface.getGeometry().getInteriorRingN(0),
+                surface.getSlope(),
+                intersection
+        );
     }
 
     private Double heightAtCoordinate(ICAOAnnex14SurfaceStrip surface, Coordinate intersection){
@@ -69,5 +66,31 @@ public class ICAOAnnex14SurfaceHeightsHelper {
 
     private Double heightAtCoordinate(ICAOAnnex14SurfaceInnerHorizontal surface, Coordinate intersection){
         return surface.getHeight();
+    }
+
+    private Double heightAtCoordinate(ICAOAnnex14SurfaceTransitional surface, Coordinate intersection){
+
+        Point point = new GeometryFactory().createPoint(intersection);
+
+        return IntStream.rangeClosed(0, surface.getGeometry().getNumGeometries())
+                .boxed()
+                .map( i -> surface.getGeometry().getGeometryN(i))
+                .filter( g -> g.covers(point))
+                .findAny()
+                .map(g -> heightOnSloping(g, surface.getSlope(), intersection))
+                .orElseThrow(()-> new SigoException("No geometry covers the object"));
+    }
+
+    private Double heightOnSloping(Geometry geometry, Double slope, Coordinate intersection){
+
+        Point point = new GeometryFactory().createPoint(intersection);
+
+        double adjacent = geometry.distance(point) * 100000;
+
+        double degrees = Math.atan(slope / 100);
+
+        double hypotenuse = adjacent / Math.cos(degrees);
+
+        return Math.sqrt(Math.pow(hypotenuse,2) - Math.pow(adjacent,2));
     }
 }
